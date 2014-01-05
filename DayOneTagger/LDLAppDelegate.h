aa6fbe601cea5d5ce938dae37a435de1c40d4c10
Brock Boland <brock@brockboland.com>
2014-01-04 21:25:27 -0600

    Initial Commit

---
+++ DayOneTagger/LDLAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  LDLAppDelegate.h
+//  DayOneTagger
+//
+//  Created by Brock Boland on 1/4/14.
+//  Copyright (c) 2014 Lucky Dog Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LDLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ DayOneTagger/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DayOneTagger
+//
+//  Created by Brock Boland on 1/4/14.
+//  Copyright (c) 2014 Lucky Dog Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
